9d226691341a8a69c988fe7ca5edd1ecaacc58b5
JX <jingxiangmo@gmail.com>
2025-01-15 22:40:06 -0800

    Content updates

--- src/zbot/SpecSection.tsx
+++ src/zbot/SpecSection.tsx
@@ -10,20 +10,19 @@ const SpecSection = () => {
       <motion.aside className="col-span-full sm:col-span-5 md:col-span-6 lg:col-span-4 xl:col-span-4 2xl:col-span-6 4xl:col-span-5 flex flex-col gap-8 row-span-full">
         <div className="lg:sticky top-32">
           <hgroup>
-            <span className="text-heading-md text-foreground60 font-apparat">What is Z-Bot?</span>
+            <span className="text-heading-md text-foreground60 font-apparat">What is ZerothBot?</span>
             <h2 className="text-heading-md">
-              A general-purpose humanoid robot optimized for the K-Scale software stack.
+              The world's most accessible Physical AI for developers.
             </h2>
           </hgroup>
-          <InlineCTA href="https://docs.kscale.dev/robot/intro">
+          {/* <InlineCTA href="https://docs.kscale.dev/robot/intro">
             Read tech specs <ExpressiveArrow size="size-4" />
-          </InlineCTA>
+          </InlineCTA> */}
         </div>
       </motion.aside>
       <article className="col-span-full lg:col-span-5 2xl:col-span-6 2xl:-col-end-1 4xl:col-span-7 4xl:-col-end-1 grid grid-cols-subgrid gap-y-4">
         <h3 className="col-span-full sm:col-span-5 sm:col-start-2 md:col-span-5 md:col-start-4 lg:col-span-4 lg:col-start-2 xl:col-span-3 xl:col-start-2 2xl:col-span-5 2xl:col-start-2 3xl:col-span-5 3xl:col-start-2 4xl:col-span-4 4xl:col-start-2 text-heading-sm">
-          We built Z-Bot with an aerospace-grade aluminium body, anodized in a protective matte
-          black finish.
+            <span className="text-rust">Open-source AI.</span> Capable of learning-based control and running the latest and most advanced end-to-end AI/ML models.
         </h3>
         <figure className="col-span-full sm:col-span-5 sm:col-start-2 md:col-span-5 md:col-start-4 lg:col-span-4 lg:col-start-2 xl:col-span-3 xl:col-start-2 2xl:col-span-5 2xl:col-start-2 3xl:col-span-5 3xl:col-start-2 4xl:col-span-4 4xl:col-start-2 aspect-video relative rounded-md overflow-hidden">
           <Image
@@ -39,8 +38,7 @@ const SpecSection = () => {
       </article>
       <article className="col-span-full lg:col-span-5 2xl:col-span-6 2xl:-col-end-1 4xl:col-span-7 4xl:-col-end-1 grid grid-cols-subgrid gap-y-4">
         <h3 className="col-span-full sm:col-span-5 sm:col-start-2 md:col-span-5 md:col-start-4 lg:col-span-4 lg:col-start-2 xl:col-span-3 xl:col-start-2 2xl:col-span-5 2xl:col-start-2 3xl:col-span-5 3xl:col-start-2 4xl:col-span-4 4xl:col-start-2 text-heading-sm">
-          Equipped with high-torque quasi-direct drive electric actuators running high-frequency
-          closed-loop control.
+          <span className="text-rust">Open-source SDK.</span> Zeroth Bot is built on top of the K-Scale software stack provides Python SDK for developers.
         </h3>
         <figure className="col-span-full sm:col-span-5 sm:col-start-2 md:col-span-5 md:col-start-4 lg:col-span-4 lg:col-start-2 xl:col-span-3 xl:col-start-2 2xl:col-span-5 2xl:col-start-2 3xl:col-span-5 3xl:col-start-2 4xl:col-span-4 4xl:col-start-2 aspect-video relative rounded-md overflow-hidden">
           <Image
@@ -56,8 +54,7 @@ const SpecSection = () => {
       </article>
       <article className="col-span-full lg:col-span-5 2xl:col-span-6 2xl:-col-end-1 4xl:col-span-7 4xl:-col-end-1 grid grid-cols-subgrid gap-y-4">
         <h3 className="col-span-full sm:col-span-5 sm:col-start-2 md:col-span-5 md:col-start-4 lg:col-span-4 lg:col-start-2 xl:col-span-3 xl:col-start-2 2xl:col-span-5 2xl:col-start-2 3xl:col-span-5 3xl:col-start-2 4xl:col-span-4 4xl:col-start-2 text-heading-sm">
-          Dependable, blazing-fast end-to-end neural network inference with our custom
-          high-performance operating system.
+        <span className="text-rust">Open-source community.</span> Join the community of world-class developers and researchers that share their data, models, and code.
         </h3>
         <figure className="col-span-full sm:col-span-5 sm:col-start-2 md:col-span-5 md:col-start-4 lg:col-span-4 lg:col-start-2 xl:col-span-3 xl:col-start-2 2xl:col-span-5 2xl:col-start-2 3xl:col-span-5 3xl:col-start-2 4xl:col-span-4 4xl:col-start-2 aspect-video relative rounded-md overflow-hidden">
           <Image
