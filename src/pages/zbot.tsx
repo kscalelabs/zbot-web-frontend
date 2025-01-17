8d42cc5eb6bce84104a2a314ef229a63c7974d24
JX <65676392+jingxiangmo@users.noreply.github.com>
2025-01-16 21:58:10 -0800

    Revert "feat(zbot): add Mailchimp integration and scripts"
    This reverts commit 5a15ef2a1b44cf3ce10e36ab5c2402da03f483bb.

--- src/pages/zbot.tsx
+++ src/pages/zbot.tsx
@@ -1,16 +1,16 @@
 import Footer from "@/components/footer/footer";
-import NavBar from "@/components/navbar/navbar";
-import CommunitySection from "@/zbot/CommunitySection";
-import GallerySection from "@/zbot/GallerySection";
-import HeaderSection from "@/zbot/HeaderSection";
-import ResearchSection from "@/zbot/ResearchSection";
-import RobotSection from "@/zbot/RobotSection";
-import SpecSection from "@/zbot/SpecSection";
+import NavBar from "@/components/zbot/navbar/navbar";
+import CommunitySection from "@/landing/CommunitySection";
+import GallerySection from "@/landing/GallerySection";
+import HeaderSection from "@/components/zbot/header";
+import ResearchSection from "@/landing/ResearchSection";
+import RobotSection from "@/landing/RobotSection";
+import SpecSection from "@/landing/SpecSection";
+import Sponsors from "@/landing/Sponsors";
 import { useLenis } from "lenis/dist/lenis-react";
 import { useEffect } from "react";
-import Script from "next/script";
 
-export default function ZBot() {
+export default function Home() {
   const lenis = useLenis();
 
   useEffect(() => {
@@ -39,16 +39,12 @@ export default function ZBot() {
         <HeaderSection />
         <ResearchSection />
         <SpecSection />
-        {/* <RobotSection /> */}
+        <RobotSection />
         <GallerySection />
         <CommunitySection />
+        {/* <Sponsors /> */}
       </main>
       <Footer />
-      <Script src="https://code.jquery.com/jquery-3.7.1.min.js" strategy="beforeInteractive" />
-      <Script src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js" strategy="afterInteractive" />
-      <Script id="mailchimp-init" strategy="afterInteractive">
-        {`(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);`}
-      </Script>
     </div>
   );
 }
