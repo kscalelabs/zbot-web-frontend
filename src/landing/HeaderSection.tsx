fd4d945520c3b019aa35053646e48a667c1762ec
JX <65676392+jingxiangmo@users.noreply.github.com>
2025-01-16 22:54:50 -0800

    Revert "Revert "feat: adjust star counter settings""
    This reverts commit 5fd998e725fa7836719dbefd245fbfd93e11e634.

--- src/landing/HeaderSection.tsx
+++ src/landing/HeaderSection.tsx
@@ -117,7 +117,7 @@ const HeaderSection = () => {
         left: 50%;
         transform: translateX(-50%);
         font-size: 24px;
-        color: white;
+        color: #808080;
         z-index: 100;
         transition: transform 0.3s;
       `;
@@ -126,9 +126,9 @@ const HeaderSection = () => {
 
     // Define score thresholds and effects
     const SCORE_THRESHOLDS = {
-      START: 30,
-      WOBBLE: 40,
-      CHAOS: 70,
+      START: 50,
+      WOBBLE: 60,
+      CHAOS: 90,
       WIN: 100,
     };
 
@@ -148,7 +148,7 @@ const HeaderSection = () => {
         scoreElement.style.transform = "translateX(-50%)";
       }
 
-      scoreElement.textContent = `Stars: ${score}`;
+      scoreElement.textContent = `🗿 Stars: ${score}`;
     };
 
     const checkVictory = () => {
