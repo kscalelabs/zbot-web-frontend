92459109f25d38e937a5aea6b7908625fb497f8f
Deborah Wang <dqw@sfu.ca>
2025-02-20 20:32:34 -0800

    add global.d.ts

--- src/gtm/TagNames.tsx
+++ src/gtm/TagNames.tsx
@@ -6,3 +6,13 @@ export const GTMTagNames = {
   NAV_CTA_PREORDER_BTN: "nav_preorder_btn_click",
   HEADER_CTA_PREORDER_BTN: "header_preorder_btn_click",
 };
+
+export const GTMTags = (eventName: string, label: string, action: string) => {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: eventName,
+    category: "User Interaction",
+    action: action, // Use the action parameter passed to the function
+    label: label,
+  });
+};
