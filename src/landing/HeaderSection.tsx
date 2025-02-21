952feca2c299593e15ca21ec7d369b92b1d92d43
Deborah Wang <dqw@sfu.ca>
2025-02-20 19:23:32 -0800

    update vercel blob

--- src/landing/HeaderSection.tsx
+++ src/landing/HeaderSection.tsx
@@ -2,6 +2,9 @@ import { CTAButton } from "@/components/buttons/CTAButtons";
 import { ColorVariant, FillMode, Size } from "@/components/util/constants";
 
 const HeaderSection = () => {
+  const videoUrl: string = "./video/header_render_video.mp4";
+  const vercelUrl: string = "";
+
   return (
     <section
       className="col-span-full grid grid-cols-subgrid min-h-[100svh] auto-rows-auto items-end text-foreground
@@ -16,7 +19,7 @@ const HeaderSection = () => {
         muted={true}
         preload="auto"
       >
-        <source src="./video/header_render_video.mp4" type="video/mp4" />
+        <source src={videoUrl} type="video/mp4" />
       </video>
 
       <div className="z-10 col-span-full flex gap-5 justify-between md:items-end flex-col md:flex-row">
